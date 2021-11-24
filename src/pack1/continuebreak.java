09685462ec72220537649a7a41cc27d219459bd6
iamharishwar <harish.sedam07@gmail.com>
2021-11-24 15:51:08 +0100

    while , conditions and do while loops

---
+++ src/pack1/continuebreak.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class continuebreak {
+
+    public static void main(String[] arg){
+
+        for(int i=1;i<=5;i++){
+
+            if(i==3){
+                break;
+            }
+            System.out.println("the value of i is:" + i);
+        }
+    }
+}
---
+++ src/pack1/forloop.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class forloop {
+
+    public static void main(String[] arg){
+
+        for(int i=0;i<=2;i++){
+            System.out.println("for loop executed");
+        }
+
+    }
+}
