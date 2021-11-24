b9fe06a840c41b15195fa14553555a6c299ff9b9
iamharishwar <harish.sedam07@gmail.com>
2021-11-24 15:19:35 +0100

    while , conditions and do while loops

---
+++ src/pack1/condition.java
@@ -0,0 +1,20 @@
+package pack1;
+
+public class condition {
+
+    public static void main(String[] arg){
+
+        int n=10;
+        int j=0;
+
+        if(n==1){
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+
+         j = n>0?5:20;
+    }
+}
---
+++ src/pack1/loops.java
@@ -0,0 +1,18 @@
+package pack1;
+
+public class loops {
+
+
+    public static void main(String[] arg){
+
+        int i=0;
+        while(i<=10){
+            System.out.println("while");
+            i++;
+        }
+        int j=0;
+        do{
+                System.out.println("do while");
+        }while(j>100);
+    }
+}
