27313a97ebf92c422f64a5295612ebabff14e4e4
iamharishwar <harish.sedam07@gmail.com>
2021-11-24 12:39:35 +0100

    started java revision

---
+++ src/pack1/cls1.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class cls1 {
+
+    public static void add(int a, int b){
+        int result  = a+b;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args){
+
+        cls1 name = new cls1();
+        name.add(100,200);
+        System.out.println("hallo world");
+    }
+}
