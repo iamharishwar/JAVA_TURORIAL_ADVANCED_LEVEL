5b693f959512f100f409b84e92f5e413fe18861d
iamharishwar <harish.sedam07@gmail.com>
2021-11-24 14:29:31 +0100

    SWITCH CASE EXAMPLE

---
+++ src/pack1/class3.java
@@ -0,0 +1,30 @@
+package pack1;
+
+public class class3 {
+
+
+    public static void main(String[] args){
+      int  n = 5;
+
+        switch(n){
+            case 1:
+                System.out.print("one");
+                break;
+            case 2:
+                System.out.print("two");
+                break;
+            case 3:
+                System.out.print("three");
+                break;
+            case 4:
+                System.out.print("four");
+                break;
+            case 5:
+                System.out.print("five");
+                break;
+            default:
+                System.out.print("no matching");
+                break;
+        }
+    }
+}
